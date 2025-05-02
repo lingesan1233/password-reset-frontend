119b9bff52862d9e7445e1e6c427179e1f885765
lingesan <sglingesan@gmail.com>
2025-05-02 12:14:05 +0530

    ff

--- frontend/src/pages/Home.jsx
+++
@@ -1,6 +0,0 @@
-function Home() {
-    return <h1 className="p-4 text-2xl">Welcome to the Home Page</h1>
-  }
-  
-  export default Home
-  
--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
-})
-
-export const signup = (data) => API.post('/signup', data)
-export const login = (data) => API.post('/login', data)
-export const resetPassword = (data) => API.post('/reset-password', data)
