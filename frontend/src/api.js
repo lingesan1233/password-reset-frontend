f9e7e3171e70e40a78b8eaf59fb9ce02943744d5
lingesan <sglingesan@gmail.com>
2025-05-07 10:04:02 +0530

    2

--- frontend/src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const API = import.meta.env.VITE_API_URL;
-
-export const signup = (data) => axios.post(`${API}/signup`, data);
-export const login = (data) => axios.post(`${API}/login`, data);
-export const reset = (data) => axios.post(`${API}/reset`, data);
